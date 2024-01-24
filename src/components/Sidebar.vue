ba7d5dd44933f86eae34ef8d1c623dcaeddaa640
Gabriel <gabrielsouzacontato.99@gmail.com>
2024-01-24 16:21:15 -0300

    Definição de sidebar

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <nav>
+          <ul>
+            <li><router-link to="/">Home</router-link></li>
+            <li><router-link to="/about">About</router-link></li>
+          </ul>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+div{
+  padding: 1vh 1vw;
+}
+nav{
+  background-color: rgb(145, 47, 145);
+  color: white;
+  width: 20rem;
+  height: 90vh;
+  z-index: 1;
+  border-radius: 20px;
+  padding: 3rem;
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <p class="text-lg">About View</p>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'AboutView',
+}
+</script>
